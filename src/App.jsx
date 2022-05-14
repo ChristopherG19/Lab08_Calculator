183f7813dd78c9c48d2741d9ca5119a046cc1b9b
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-05-14 11:04:38 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Calcu from './components/Calculator'
+
+const App = () => {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>CALCULATOR UVG</h1>
+      </header>
+      
+      <Calcu />  
+      
+    </div>
+  )
+}
+
+export default App
