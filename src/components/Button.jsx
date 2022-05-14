05e084e2143b188e6504d34090895fc59209bdb8
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-05-14 11:04:53 -0600

    Primer commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css'
+
+const ButtonsCal = ({ info }) => {
+    return (
+        <button className='button-calc'>
+            {info}
+        </button>
+    )
+}
+
+
+export default ButtonsCal
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './Calculator.css'
+import ButtonsCal from './Button'
+
+const buttons = ["AC", "DEL", "/", "X", "1", "2", "3", "-", "4", "5", "6", "+", "7", "8", "9", "0"]
+const equal = "="
+
+const Calcu = () => {
+
+	const [operationText, setOperationText] = useState("")
+
+	return (
+		<div className='calcuBack'>
+			<div className='displayBack'>
+				<div className='displayT'>
+					{operationText}
+				</div>
+			</div>
+			<div className='gridButtons'>
+				{buttons.map((element, index) => (
+					<ButtonsCal 
+						key={index}
+						info={element}
+					/>
+				))}
+			</div>
+			<div className='equal'>
+				<ButtonsCal info={equal}/>
+			</div>
+		</div>
+	)
+}
+
+export default Calcu
