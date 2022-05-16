6a01d4646d0a49d4c784ceb9d442aca28a017ad9
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-05-15 22:32:42 -0600

    Integración de Linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
 import Calcu from './components/Calculator'
 
-const App = () => {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>CALCULADORA UVG</h1>
-      </header>
-      <Calcu />  
-    </div>
-  )
-}
+const App = () => (
+  <div className="app">
+    <header className="app-header">
+      <h1>CALCULADORA UVG</h1>
+    </header>
+    <Calcu />
+  </div>
+)
 
 export default App
